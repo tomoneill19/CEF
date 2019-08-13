573e59a6f543f4a0ac8709d26f8dcb2d1c67b1d4
Tom O'Neill <tomoneill19@gmail.com>
2019-08-13 11:32:11 +0100

    added arp scanner for host discovery + added arp scan + updated help and usage for new command

--- framework.py
+++ framework.py
@@ -31,12 +31,12 @@ banner = r'''
  \______/ |________/|__/
                           '''
 
-validCommands = ["scan", "scannames", "hosts", "credtest", "getcmd", "rexec", "rcpy", "msg", "add", "remove", "intel", "source"]
+validCommands = ["scan", "scannames", "scanarp", "hosts", "credtest", "getcmd", "rexec", "rcpy", "msg", "add", "remove", "intel", "source"]
 validDesc = [
-    "Run a ping scan to identify hosts on the network", "Run a scan to find all the hosts on the network using netbios name", "List all hosts stored on this device", "Test to see if default creds work", "Open a shell on a remote system (user / pass)", "Run a command on a single or a group of PCs (add default to use 'Default' list)", "rexec but copy and execute a file from this system", "Message a single or group of computers", "Add an IP to a list", "Remove an IP from a list",
+    "Run a ping scan to identify hosts on the network", "Run a scan to find all the hosts on the network using netbios name", "Run an arp scan to identify hosts on the network", "List all hosts stored on this device", "Test to see if default creds work", "Open a shell on a remote system (user / pass)", "Run a command on a single or a group of PCs (add default to use 'Default' list)", "rexec but copy and execute a file from this system", "Message a single or group of computers", "Add an IP to a list", "Remove an IP from a list",
     "View or edit intel on a given IP", "Edit the list of shared intel sources"
 ]
-validUsage = ["-", "-", "hosts ([update])", "credtest [username:password] [list name to save under]", "getcmd [ip] [username] [password]", "rexec [list name] [username:password] [command]", "rcpy [list name] [username:password] [payload name] ([remote])", "msg [list name] [num times] ", "add [ip] [list]", "remove [ip] [list]", "intel [ip] ([add/remove] [information to add/remove])", "source [add/del/list] ([ip])"]
+validUsage = ["-", "-", "-", "hosts ([update])", "credtest [username:password] [list name to save under]", "getcmd [ip] [username] [password]", "rexec [list name] [username:password] [command]", "rcpy [list name] [username:password] [payload name] ([remote])", "msg [list name] [num times] ", "add [ip] [list]", "remove [ip] [list]", "intel [ip] ([add/remove] [information to add/remove])", "source [add/del/list] ([ip])"]
 
 ipNames = []
 customLists = []
@@ -404,6 +404,10 @@ def menu():
             rscannames()
             beep()
 
+        if cmd[0] == "scanarp":
+            rscanarp()
+            beep()
+
         if cmd[0] == "hosts":
             for x in range(0, len(ipNames)):
                 if len(customLists[x]) == 0:
@@ -666,6 +670,37 @@ def rscannames():  # Conducts a scan of the netbios names to discover any hosts 
     print("\n[+] DISCOVERED: " + str(len(customLists[0])))
     print("\n[+] RANGE: " + str(customLists[0][0]) + " -> " + str(customLists[0][-1]))
     print("\n==================================================\n")
+
+
+def rscanarp():  # Conducts an arp scan to discover any hosts on the network
+    global customLists
+    if len(customLists) == 0:
+        customLists.append([])
+    if len(ipNames) == 0:
+        ipNames.append("ips")
+    else:
+        customLists[0] = []
+    os.system("arp -a -n " + socket.gethostbyname(socket.gethostname()) + " > arp.txt")
+    with open("arp.txt", "r") as file:
+        for line in file:
+            try:
+                temp = line.split(" ")
+                print(temp)
+                temp2 = temp[2].split(".")
+                print(temp2)
+                if "static" not in temp:
+                    customLists[0].append(temp2[3])
+            except IndexError:
+                pass
+    print(customLists[0])
+    os.remove("arp.txt")
+    os.system("cls")
+    print("\n==================================================")
+    print("\n[+] HOST SCAN COMPLETE")
+    print("\n[+] DISCOVERED: " + str(len(customLists[0])))
+    print("\n[+] RANGE: " + str(customLists[0][0]) + " -> " + str(customLists[0][-1]))
+    print("\n==================================================\n")
+    intelWrite()
 
 
 def rmsg(targets, reason, num):
